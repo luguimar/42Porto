560852868404dd16a2b1cb1bccf1bf03ff2ef55f
Luis Gustavo Guimaraes Cavalcante Maia De Sousa <luguimar@c1r9s4.42porto.com>
2023-06-08 22:39:43 +0100

    starting get next line

---
+++ common_core/get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luguimar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 21:46:12 by luguimar          #+#    #+#             */
+/*   Updated: 2023/06/08 21:50:22 by luguimar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
