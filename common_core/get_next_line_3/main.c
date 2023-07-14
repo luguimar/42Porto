a05a61c5922f66b3faaef4734980ed6c608952bf
Luis Gustavo Guimaraes Cavalcante Maia De Sousa <luguimar@c1r5s5.42porto.com>
2023-07-14 22:55:06 +0100

    get_next_line_3

---
+++ common_core/get_next_line_3/main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luguimar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 22:07:23 by luguimar          #+#    #+#             */
+/*   Updated: 2023/07/12 22:11:49 by luguimar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("empty.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return (1);
+	}
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (0);
+}
