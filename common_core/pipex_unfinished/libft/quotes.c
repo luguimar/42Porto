a7db853d05ec9327be3e3b8f0b73ebbf40207454
luguimar <luisgustavo999@gmail.com>
2023-10-04 18:28:37 +0100

    update pipex quotes.c

--- common_core/pipex_unfinished/libft/quotes.c
+++ common_core/pipex_unfinished/libft/quotes.c
@@ -6,7 +6,7 @@
 /*   By: luguimar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 12:55:47 by luguimar          #+#    #+#             */
-/*   Updated: 2023/10/04 15:23:46 by luguimar         ###   ########.fr       */
+/*   Updated: 2023/10/04 18:24:56 by luguimar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,24 @@ char	isquote(char *s, int index)
 	char	quote;
 
 	quote = '\0';
-	if (s[index] == inquote(s, index - 1) && !inquote(s, index))
+	if ((s[index] == inquote(s, index - 1) && !inquote(s, index))
+		|| (!inquote(s, index - 1) && s[index - 1] == s[index]
+			&& (s[index - 1] == '\"' || s[index - 1] == '\'')
+			&& !inquote(s, index - 2) && !inquote(s, index + 1)))
+	{
 		quote = inquote(s, index - 1);
-	else if (s[index] == inquote(s, index + 1) && !inquote(s, index))
+		if (quote == '\0')
+			quote = s[index - 1];
+	}
+	else if ((s[index] == inquote(s, index + 1) && !inquote(s, index))
+		|| (!inquote(s, index + 1) && s[index + 1] == s[index]
+			&& (s[index + 1] == '\"' || s[index + 1] == '\'')
+			&& !inquote(s, index + 2) && !inquote(s, index - 1)))
+	{
 		quote = inquote(s, index + 1);
+		if (quote == '\0')
+			quote = s[index + 1];
+	}
 	return (quote);
 }
 
